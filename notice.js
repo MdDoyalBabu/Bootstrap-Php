51c7af9a80f1280ca94ef51ea0c64645c0ee1506
MdDoyalBabu <mddoyalbabucmt@gmail.com>
2021-06-03 12:10:01 +0600

    Php Project

---
+++ notice.js
@@ -0,0 +1,13 @@
+var myVar=document.querySelector("#id1");
+function check(){
+    var email=prompt("Email Addresss");
+    var pwd=prompt("Password");
+
+    if((email=="fd.doyal@gmail.com")&&(pwd==44431962))
+    {
+        alert("notice.html");
+    }
+    else{
+        alert("login UnSuccessful");
+    }
+}
